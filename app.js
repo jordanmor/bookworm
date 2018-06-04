e062f709e9cf2193236c1feabbd2d74157bad967
Jordan Mor <jordanmor@gmail.com>
2018-06-04 14:13:29 -0400

    Add app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+//view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(3000, function() {
+    console.log('The application is running on localhost:3000');
+});
