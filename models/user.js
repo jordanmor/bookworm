fd6ad607d5530a8ae73d1d0840cf77e3f8d75584
Jordan Mor <jordanmor@gmail.com>
2018-06-05 12:40:50 -0400

    Define mongoose schema

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    name: {
+        type:String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    favoriteBook: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
