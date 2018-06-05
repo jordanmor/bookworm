6b3c13ee3fb145e289085d8c511e378a23c9d9cb
Jordan Mor <jordanmor@gmail.com>
2018-06-05 09:39:44 -0400

    Add and style About page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {title: 'Home'});
+    res.render('index', {title: 'Home'} );
+});
+
+router.get('/about', (req, res) => {
+    res.render('about', {title: 'About'} );
 });
 
 module.exports = router;
