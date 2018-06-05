fa93ffb2dfd6a1c083c58a458a545f3d4770e4af
Jordan Mor <jordanmor@gmail.com>
2018-06-05 16:02:14 -0400

    Define a mongoose pre hook and use bcrypt to hash passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: {
@@ -22,6 +23,16 @@ const UserSchema = new Schema({
         required: true
     }
 });
-
+// hash password before saving to database
+UserSchema.pre('save', function (next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            next(err);
+        }
+        user.password = hash;
+        next();
+    });
+});
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
