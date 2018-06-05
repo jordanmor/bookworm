af7badaf97fd86fc6871c347a641fd723539bd7a
Jordan Mor <jordanmor@gmail.com>
2018-06-05 09:50:33 -0400

    Add Contact page, style and add route

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,8 @@ router.get('/about', (req, res) => {
     res.render('about', {title: 'About'} );
 });
 
+router.get('/contact', (req, res) => {
+    res.render('contact', {title: 'contact'} );
+});
+
 module.exports = router;
